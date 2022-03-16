7c98cb5af8436845bb4e54b169fb6a28a951677b
David Asensio Cañas <david.asensio@icloud.com>
2022-03-16 18:04:43 +0100

    Refactor serializer to serialize pairs of account numbers with status

--- src/serializer.js
+++ src/serializer.js
@@ -1,17 +1,42 @@
 const R = require('ramda');
 
+const {
+  VALID_NUMBER_STATUS,
+  INVALID_NUMBER_STATUS,
+  ILLEGIBLE_NUMBER_STATUS
+} = require('./constants');
+
 const INVALID_DIGIT_PLACEHOLDER = '?'
 
+const STATUS_TO_STRING_MAP = {
+  [VALID_NUMBER_STATUS]: '',
+  [INVALID_NUMBER_STATUS]: ' ERR',
+  [ILLEGIBLE_NUMBER_STATUS]: ' ILL',
+}
+
 const serializer = (function createSerializer() {
-  const serializeDigit = R.when(
+  const serializeOneDigit = R.when(
     R.isNil(),
     R.always(INVALID_DIGIT_PLACEHOLDER)
   )
 
-  const serialize = R.pipe(
-    R.map(serializeDigit),
+  const serializeDigits = R.pipe(
+    R.map(serializeOneDigit),
     R.join('')
   )
+
+  const serializeStatus = R.partialRight(
+    R.prop,
+    [STATUS_TO_STRING_MAP]
+  )
+
+  const serializeOne = ([accountNumber, status]) =>
+    R.concat(
+      serializeDigits(accountNumber),
+      serializeStatus(status)
+    )
+
+  const serialize = R.map(serializeOne)
 
   return {
     serialize
--- src/serializer.test.js
+++ src/serializer.test.js
@@ -1,25 +1,23 @@
 const { serializer } = require("./serializer")
 
-it('should serialize a legible accountNumber', () => {
-  const accountNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const {
+  VALID_NUMBER_STATUS,
+  INVALID_NUMBER_STATUS,
+  ILLEGIBLE_NUMBER_STATUS
+} = require('./constants');
 
-  const result = serializer.serialize(accountNumber)
+it('should serialize pairs of account numbers with status', () => {
+  const accountNumbersWithStatus = [
+    [[1, 2, 3, 4, 5, 6, 7, 8, 9],       VALID_NUMBER_STATUS],
+    [[1, 1, 1, 1, 1, 1, 1, 1, 1],       INVALID_NUMBER_STATUS],
+    [[1, null, 5, 1, 3, null, 1, 4, 6], ILLEGIBLE_NUMBER_STATUS]
+  ]
 
-  expect(result).toBe('123456789')
-})
+  const result = serializer.serialize(accountNumbersWithStatus)
 
-it('should serialize a legible accountNumber containing zeros', () => {
-  const accountNumber = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-  const result = serializer.serialize(accountNumber)
-
-  expect(result).toBe('000000000')
-})
-
-it('should serialize a illegible accountNumber', () => {
-  const accountNumber = [1, 2, null, 4, 5, null, 7, 8, null]
-
-  const result = serializer.serialize(accountNumber)
-
-  expect(result).toBe('12?45?78?')
+  expect(result).toStrictEqual([
+    '123456789',
+    '111111111 ERR',
+    '1?513?146 ILL'
+  ])
 })
