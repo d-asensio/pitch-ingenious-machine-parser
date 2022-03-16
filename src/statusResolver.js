39d597ec7b0ec22961611aca51372ce9e1303ffb
David Asensio Cañas <david.asensio@icloud.com>
2022-03-16 14:39:27 +0100

    The account number status can be resolved using a resolver

---
+++ src/statusResolver.js
@@ -0,0 +1,47 @@
+const R = require('ramda');
+
+const { validator: defaultValidator } = require('./validator')
+
+const {
+  VALID_NUMBER_STATUS,
+  INVALID_NUMBER_STATUS,
+  ILLEGIBLE_NUMBER_STATUS
+} = require('./constants');
+
+function createStatusResolver(dependencies = {}) {
+  const {
+    validator = defaultValidator
+  } = dependencies;
+
+  const isIllegibleAccountNumber = R.includes(null);
+
+  const isInvalidAccountNumber = R.complement(validator.validate);
+
+  const resolveIllegibleStatus = R.ifElse(
+    isIllegibleAccountNumber,
+    R.always(ILLEGIBLE_NUMBER_STATUS),
+    R.always(false)
+  )
+
+  const resolveInvalidStatus = R.ifElse(
+    isInvalidAccountNumber,
+    R.always(INVALID_NUMBER_STATUS),
+    R.always(false)
+  )
+
+  const resolve = R.either(
+    R.either(
+      resolveIllegibleStatus,
+      resolveInvalidStatus,
+    ),
+    R.always(VALID_NUMBER_STATUS)
+  )
+
+  return {
+    resolve
+  }
+}
+
+module.exports = {
+  createStatusResolver
+}
---
+++ src/statusResolver.test.js
@@ -0,0 +1,44 @@
+const { when } = require('jest-when');
+const {
+  VALID_NUMBER_STATUS,
+  INVALID_NUMBER_STATUS,
+  ILLEGIBLE_NUMBER_STATUS
+} = require('./constants')
+
+const { createStatusResolver } = require("./statusResolver")
+
+const validator = {
+  validate: jest.fn()
+}
+
+const statusResolver = createStatusResolver({ validator })
+
+it('should resolve the status to "ILLEGIBLE_NUMBER_STATUS" for an illegible account number', () => {
+  const accountNumber = [1, 2, null, 4, 5, null, 7, 8, null]
+
+  const result = statusResolver.resolve(accountNumber)
+
+  expect(result).toBe(ILLEGIBLE_NUMBER_STATUS)
+})
+
+it('should resolve the status to "INVALID_NUMBER_STATUS" when it is unable to validate the account number using the validator', () => {
+  const accountNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  when(validator.validate)
+    .calledWith(accountNumber)
+    .mockReturnValue(false)
+
+  const result = statusResolver.resolve(accountNumber)
+
+  expect(result).toBe(INVALID_NUMBER_STATUS)
+})
+
+it('should resolve the status to "VALID_NUMBER_STATUS" when the validator takes the account number as valid', () => {
+  const accountNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  when(validator.validate)
+    .calledWith(accountNumber)
+    .mockReturnValue(true)
+
+  const result = statusResolver.resolve(accountNumber)
+
+  expect(result).toBe(VALID_NUMBER_STATUS)
+})
